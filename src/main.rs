ca9569d1bca71908857ee8b1c85a8f142106f07e
Hannes Reinecke <hare@suse.de>
2025-11-11 09:48:08 +0100

    s3gw: simple web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::{
+    io::{BufReader, prelude::*},
+    net::{TcpListener, TcpStream},
+};
+use http::{Request, Response, StatusCode};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("localhost:7878").unwrap();
+
+    for stream in listener.incoming() {
+	let stream = stream.unwrap();
+
+	handle_connection(stream);
+    }
+}
+
+fn handle_connection(stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+	.lines()
+	.map(|result| result.unwrap())
+	.take_while(|line| !line.is_empty())
+	.collect();
+    let request: Request<Vec<_>> = http_request.into();
+
+    let mut response: Response<_> = respond_to(request);
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
+
+fn respond_to(req: Request<()>) -> http::Result<Response<()>> {
+    let mut builder = Response::builder()
+	.header("Location","eu-west-1")
+	.status(StatusCode::OK);
+
+    builder.body(())
 }
