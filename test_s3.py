8a9307f72bb59179eefb4808cb3d245407e77c06
Hannes Reinecke <hare@suse.de>
2025-11-13 09:13:43 +0100

    test_s3: simple boto3 client

---
+++ test_s3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.11
+
+import boto3
+
+session = boto3.session.Session()
+
+s3_client = session.client(
+    service_name='s3',
+    aws_access_key_id='aaa',
+    aws_secret_access_key='bbb',
+    endpoint_url='https://localhost:7878',
+    verify=False,
+    )
+
+for bucket in s3_client.list_buckets():
+    print(f'{bucket}')
