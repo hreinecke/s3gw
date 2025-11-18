5497eb15ac2626ef238db0fb9daffb96553c2962
Hannes Reinecke <hare@suse.de>
2025-11-18 12:29:56 +0100

    object: dynamically generate ListObjects response

--- object.c
+++ object.c
@@ -13,60 +13,52 @@
 #include "s3_api.h"
 #include "s3gw.h"
 
-static char list_objects_preamble[] =
-	"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n"
-	"<ListBucketResult xmlns=\"http://s3.amazonaws.com/doc/2006-03-01/\">\r\n"
-	"<Name>%s</Name>\r\n"
-	"%s%s%s\r\n"
-	"<Marker/>\r\n"
-	"<MaxKeys>%lu</MaxKeys>\r\n"
-	"<IsTruncated>%s</IsTruncated>\r\n";
-static char list_objects_template[] =
-	"<Contents>\r\n"
-	"<Key>%s</Key>\r\n"
-	"<LastModified>%s</LastModified>\r\n"
-	"<ETag>\"%02x%02x%02x%02x%02x%02x%02x"
-	"%02x%02x%02x%02x%02x%02x%02x%02x%02x\"</ETag>\r\n"
-	"<Size>%lu</Size>\r\n"
-	"<StorageClass>STANDARD</StorageClass>\r\n"
-	"<Owner>\r\n<ID>%s</ID>\r\n</Owner>\r\n"
-	"</Contents>\r\n";
-static char list_objects_postamble[] =
-	"<KeyCount>%d</KeyCount>\r\n"
-	"</ListBucketResult>\r\n";
+static xmlChar xmlns[] =
+	"xmlns=\"http://s3.amazonaws.com/doc/2006-03-01/\"";
 
 char *list_objects(struct s3gw_request *req, int *outlen)
 {
 	struct linked_list top;
 	struct s3gw_object *o, *t;
-	struct s3gw_header *hdr;
-	char *buf, *prefix = NULL;
-	size_t off = 0, total = 4096, hlen;
+	struct s3gw_header *q;
+	xmlDoc *doc;
+	xmlNsPtr ns;
+	xmlNode *root_node, *c_node, *o_node;
+	char *buf, *prefix;
+	xmlChar *xml;
+	char *line;
 	enum http_status s = HTTP_STATUS_OK;
-	int ret, num;
-	unsigned long max_keys = 200;
+	int ret, cur = 0, num, line_len, xml_len;
+	unsigned long max_keys = 0;
 
-	list_for_each_entry(hdr, &req->query_list, list) {
-		if (!strcmp(hdr->key, "max-keys")) {
-			max_keys = strtoul(hdr->value, NULL, 10);
+	list_for_each_entry(q, &req->query_list, list) {
+		if (!strcmp(q->key, "max-keys")) {
+			max_keys = strtoul(q->value, NULL, 10);
 		}
-		if (!strcmp(hdr->key, "prefix"))
-			prefix = hdr->value;
+		if (!strcmp(q->key, "prefix"))
+			prefix = q->value;
 	}
-	buf = malloc(total);
-	if (!buf)
-		return NULL;
-	ret = snprintf(buf, total, "HTTP/1.1 %d %s\r\n"
-		       "Content-Length:     \r\n\r\n",
-		       s, http_status_str(s));
-	if (ret < 0) {
-		free(buf);
-		return NULL;
-	}
-	off += ret;
-	/* Save the header length */
-	hlen = off;
-
+	doc = xmlNewDoc((const xmlChar *)"1.0");
+	root_node = xmlNewDocNode(doc, NULL,
+				  (const xmlChar *)"ListBucketResult", NULL);
+	ns = xmlNewNs(root_node, xmlns, NULL);
+	xmlSetNs(root_node, ns);
+	xmlDocSetRootElement(doc, root_node);
+	xmlNewChild(root_node, NULL, (const xmlChar *)"Name",
+		    (xmlChar *)req->bucket);
+	xmlNewChild(root_node, NULL, (const xmlChar *)"Prefix",
+		    (xmlChar *)prefix);
+	xmlNewChild(root_node, NULL, (const xmlChar *)"Marker", NULL);
+	if (max_keys)
+		asprintf(&line, "%lu", max_keys);
+	else
+		line = NULL;
+	xmlNewChild(root_node, NULL, (const xmlChar *)"MaxKeys",
+		    (xmlChar *)line);
+	free(line);
+	xmlNewChild(root_node, NULL, (const xmlChar *)"IsTruncated",
+		    (const xmlChar *)"false");
+	     
 	INIT_LINKED_LIST(&top);
 	num = find_objects(req, &top, prefix);
 	if (num < 0) {
@@ -76,70 +68,72 @@ char *list_objects(struct s3gw_request *req, int *outlen)
 			goto out_error;
 		}
 		num = 0;
-		ret = 0;
 	}
 	printf("found %d objects\n", num);
-	ret = snprintf(buf + off, total - off, list_objects_preamble,
-		       req->bucket, prefix ? "<Prefix>" : "",
-		       prefix ? prefix : "<Prefix/>",
-		       prefix ? "</Prefix>" : "",
-		       max_keys, "false");
-	if (ret < 0) {
-		s = HTTP_STATUS_INTERNAL_SERVER_ERROR;
-		goto out_error;
-	}
-	off += ret;
 	list_for_each_entry_safe(o, t, &top, list) {
 		char time_str[64];
 		struct tm *tm;
 
 		list_del(&o->list);
-		if (off >= total) {
-			fprintf(stderr, "skip object '%s'\n",
-				o->key);
-			clear_object(o);
-			free(o);
-			continue;
+		if (max_keys && cur > max_keys) {
+			goto clear;
 		}
+		c_node = xmlNewChild(root_node, NULL,
+				     (const xmlChar *)"Contents", NULL);
+		xmlNewChild(c_node, NULL, (const xmlChar *)"Key",
+			    (xmlChar *)o->key);
 		tm = localtime(&o->mtime);
 		strftime(time_str, 64, "%FT%T%z", tm);
-		ret = snprintf(buf + off, total - off,
-			       list_objects_template,
-			       o->key, time_str,
-			       o->etag[0], o->etag[1], o->etag[2], o->etag[3],
-			       o->etag[4], o->etag[5], o->etag[6], o->etag[7],
-			       o->etag[8], o->etag[9], o->etag[10], o->etag[11],
-			       o->etag[12], o->etag[13], o->etag[14],
-			       o->etag[15], o->size, req->owner);
-		if (ret < 0)
-			fprintf(stderr, "failed to format time\n");
-		else
-			off += ret;
+		xmlNewChild(c_node, NULL,
+			    (const xmlChar *)"LastModified",
+			    (xmlChar *)time_str);
+		line = bin2hex(o->etag, 16, (size_t *)&line_len);
+		xmlNewChild(c_node, NULL,
+			    (const xmlChar *)"ETag",
+			    (xmlChar *)line);
+		free(line);
+		line_len = asprintf(&line, "%lu", o->size);
+		xmlNewChild(c_node, NULL,
+			    (const xmlChar *)"Size",
+			    (xmlChar *)line);
+		free(line);
+		xmlNewChild(c_node, NULL,
+			    (const xmlChar *)"StorageClass",
+			    (xmlChar *)"STANDARD");
+		o_node = xmlNewChild(c_node, NULL,
+			    (const xmlChar *)"Owner", NULL);
+		xmlNewChild(o_node, NULL, (const xmlChar *)"ID",
+			    (xmlChar *)req->owner);
+	clear:
 		clear_object(o);
 		free(o);
+		cur++;
 	}
-	if (off >= total) {
-		s = HTTP_STATUS_PAYLOAD_TOO_LARGE;
-		goto out_error;
-	}
-	ret = snprintf(buf + off, total - off,
-		       list_objects_postamble, num);
+	asprintf(&line, "%u", num);
+	xmlNewChild(root_node, NULL, (const xmlChar *)"KeyCount",
+		    (xmlChar *)line);
+	free(line);
+	xmlDocDumpMemory(doc, &xml, &xml_len);
+	xmlFreeDoc(doc);
+
+	ret = asprintf(&buf, "HTTP/1.1 %d %s\r\n"
+		       "Content-Length: %d\r\n\r\n%s",
+		       s, http_status_str(s), xml_len, xml);
 	if (ret < 0) {
-		s = HTTP_STATUS_INTERNAL_SERVER_ERROR;
-		goto out_error;
+		free(xml);
+		return NULL;
 	}
-	off += ret;
-	/* Fixup content length */
-	ret = snprintf(buf + hlen - 8, 5, "%4lu", off - hlen);
-	buf[hlen - 8 + ret] = '\r';
-	*outlen = off;
+	*outlen = ret;
+	free(xml);
 	return buf;
 
 out_error:
-	memset(buf, 0, off);
-	ret = sprintf(buf, "HTTP/1.1 %d %s\r\n",
-		      s, http_status_str(s));
-	*outlen = strlen(buf);
+	ret = asprintf(&buf, "HTTP/1.1 %d %s\r\n",
+		       s, http_status_str(s));
+	if (ret > 0)
+		*outlen = ret;
+	else
+		buf = NULL;
 	return buf;
 }
 
